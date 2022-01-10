d0e309eea086ea2dcd91851d7e0f22471a84f78a
knhngchn <khchen.me@gmail.com>
2022-01-10 10:08:05 +0800

    [bellmanford] rewrite, support path restoring

--- graph/bellman_ford.py
+++ graph/bellman_ford.py
@@ -1,12 +1,14 @@
 def bellman_ford(n, edges, start):
     dist = [INF for _ in range(n)]
+    prev = [-1 for _ in range(n)]
     dist[start] = 0
-    while True:
+    change = True
+    while change:
         change = False
         for (u, v, w) in edges:
             if dist[u] != INF and dist[v] > dist[u] + w:
                 dist[v] = dist[u] + w
+                prev[v] = u
                 change = True
-        if not change:
-            break
-    return dist
+
+    return dist, prev
