37a176cc505b0e6f87c5716b03d118aa5d4d1285
knhngchn <khchen.me@gmail.com>
2022-01-11 16:59:25 +0800

    add bellman-ford for negative cycle

--- graph/bellman_ford.py
+++ graph/bellman_ford.py
@@ -12,3 +12,31 @@ def bellman_ford(n, edges, start):
                 change = True
 
     return dist, prev
+
+def bellman_ford_negative_cycle(n, edges, start):
+    dist = [INF for _ in range(n)]
+    prev = [-1 for _ in range(n)]
+    dist[start] = 0
+    node = 0
+    for _ in range(n):
+        node = -1
+        for (u, v, w) in edges:
+            if dist[u] != INF and dist[v] > dist[u] + w:
+                dist[v] = max(- INF, dist[u] + w)
+                prev[v] = u
+                node = v
+
+    if node == -1:
+        return []  # no reachable negative cycle from start
+
+    for _ in range(n):
+        node = prev[node]
+    path = []
+    cur = node
+    while True:
+        path.append(cur)
+        if cur == node and len(path) > 1:
+            break
+        cur = prev[cur]
+    path.reverse()
+    return path
