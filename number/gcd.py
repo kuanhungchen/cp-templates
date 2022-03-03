9d60c58dab307ed284cd859bb2d829f79cfba307
knhngchn <khchen.me@gmail.com>
2022-03-04 03:41:25 +0800

    [gcd] update gcd for numbers

--- number/gcd.py
+++ number/gcd.py
@@ -13,22 +13,6 @@ def exgcd(a, b):
 def lcm(a, b):
     return a // gcd(a, b) * b
 
-def common_gcd(nums):
-    # O(n*sqrt(v))
-    MAXV = 10 ** 6
-    divisors = [0 for _ in range(MAXV + 5)]
-    for num in nums:
-        div = 1
-        while div * div <= num:
-            if num % div == 0:
-                divisors[div] += 1
-                divisors[num // div] += int(div * div != num)
-            div += 1
-    return next(v for v in range(MAXV, 0, -1) if divisors[v] >= 2)
-
-def common_gcd_fast(nums):
-    # O(vlogv)
-    MAXV = 10 ** 6
-    freq = [0 for _ in range(MAXV + 5)]
-    for num in nums: freq[num] += 1
-    return next(v for v in range(MAXV, 0, -1) if sum(freq[v::v]) >= 2)
+def array_gcd(nums):
+    # O(n + log(max(A)))
+    return reduce(gcd, nums)
