46a374737180c0a5d51e93c8d6358d92800054d0
kuanhungchen <khchen.me@gmail.com>
2022-03-17 18:18:08 +0800

    add Mo's algorithm

---
+++ misc/mo.py
@@ -0,0 +1,45 @@
+class Mo:
+    def __init__(self, n):
+        self.n = n
+        self.q = 0
+
+        self.queries = []
+
+    def add_query(self, ql, qr):
+        # [ql, qr]
+        self.queries.append((ql, qr))
+        self.q += 1
+
+    def solve(self):
+        def add(idx):
+            raise NotImplementedError
+
+        def remove(idx):
+            raise NotImplementedError
+
+        def out(idx):
+            raise NotImplementedError
+
+        queries = self.queries
+        SZ = 128
+        NUM = (self.n + SZ - 1) // SZ
+        B = [[] for _ in range(NUM)]
+
+        for qi, (ql, qr) in enumerate(queries):
+            B[ql // SZ].append(qi)
+
+        for bi in range(NUM):
+            B[bi].sort(key=lambda qi: queries[qi][1], reverse=bi & 1)
+
+        L, R = 0, -1
+        ans = [0 for _ in range(self.q)]
+        for qis in B:
+            for qi in qis:
+                ql, qr = queries[qi]
+                for i in range(L, ql): remove(i)
+                for i in range(R, qr, -1): remove(i)
+                for i in range(R + 1, qr + 1): add(i)
+                for i in range(L - 1, ql - 1, -1): add(i)
+                L, R = ql, qr
+                out(qi)
+        return ans
