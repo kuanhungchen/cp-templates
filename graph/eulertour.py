f8f18035c3513c660642fe1681f6554f964e4c31
kuanhungchen <knhngchn@gmail.com>
2022-05-01 11:59:08 +0800

    fix: use depth instead of euler tour

--- graph/eulertour.py
+++ graph/eulertour.py
@@ -1,10 +1,13 @@
 def eulertour(n, G, rt=None):
-    # build sparse table on euler, then LCA(u, v) = st.query(left, right + 1)
-    # where left = min(first[u], first[v]), right = max(first[u], first[v])
+    # Assume ST is sparse table built on depth array, then LCA(u, v) is
+    # ST.query(left, right + 1) & ((1 << 32) - 1),
+    # where left = min(first[u], first[v]), right = max(first[u], first[v]).
     def bfs(node):
+        dep = 0
         stk = [[node, 0]]
         first[node] = len(euler)
         euler.append(node)
+        depth.append((dep << 32) + node)
 
         while stk:
             node, idx = stk[-1]
@@ -13,24 +16,26 @@ def eulertour(n, G, rt=None):
                 stk[-1][1] += 1
                 if child == parent[node]:
                     continue
+                dep += 1
                 first[child] = len(euler)
                 euler.append(child)
+                depth.append((dep << 32) + child)
                 parent[child] = node
                 stk.append([child, 0])
             else:
                 last[node] = len(euler)
                 if parent[node] != -1:
+                    dep -= 1
                     euler.append(parent[node])
+                    depth.append((dep << 32) + parent[node])
                 stk.pop()
 
     first = [-1 for _ in range(n)]
     last = [-1 for _ in range(n)]
     parent = [-1 for _ in range(n)]
     euler = []
+    depth = []
 
-    if rt is None:
-        all(bfs(node) for node in range(n) if parent[node] == -1)
-    else:
-        bfs(rt)
-
-    return first, last, euler
+    rts = [rt] if rt is not None else range(n)
+    all(bfs(rt) for rt in rts if parent[rt] == -1)
+    return first, last, euler, depth
