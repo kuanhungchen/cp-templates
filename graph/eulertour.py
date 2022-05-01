a0c9708efd2663d5a795470a105e49378a36ccef
kuanhungchen <knhngchn@gmail.com>
2022-05-01 09:26:18 +0800

    feat: update euler tour technique

--- graph/eulertour.py
+++ graph/eulertour.py
@@ -1,47 +1,36 @@
-class EulerTour:
-    def __init__(self, n, G, root=None):
-        self.G = G
-        self.parent = [-1 for _ in range(n)]
-        self.first = [-1 for _ in range(n)]
-        self.last = [-1 for _ in range(n)]
-        self.depth = []
-        self.euler = []
+def eulertour(n, G, rt=None):
+    # build sparse table on euler, then LCA(u, v) = st.query(left, right + 1)
+    # where left = min(first[u], first[v]), right = max(first[u], first[v])
+    def bfs(node):
+        stk = [[node, 0]]
+        first[node] = len(euler)
+        euler.append(node)
 
-        if root is None: all(self.dfs(u) for u in range(n) if self.parent[u] == -1)
-        else: self.dfs(root)
-
-        self.st = SparseTable(self.depth)
-
-    def dfs(self, rt):
-        dep = 0
-        stk = [rt, 0]
-        self.first[rt] = len(self.euler)
-        self.euler.append(rt)
-        self.depth.append((dep << 32) + rt)
         while stk:
-            v, idx = stk[-2:]
-            if idx < len(self.G[v]):
-                nxtv = self.G[v][idx]
-                stk[-1] += 1
-                if nxtv == self.parent[v]:
+            node, idx = stk[-1]
+            if idx < len(G[node]):
+                child = G[node][idx]
+                stk[-1][1] += 1
+                if child == parent[node]:
                     continue
-                dep += 1
-                self.first[nxtv] = len(self.euler)
-                self.euler.append(nxtv)
-                self.depth.append((dep << 32) + nxtv)
-                self.parent[nxtv] = v
-                stk.append(nxtv)
-                stk.append(0)
+                first[child] = len(euler)
+                euler.append(child)
+                parent[child] = node
+                stk.append([child, 0])
             else:
-                self.last[v] = len(self.euler)
-                if self.parent[v] != -1:
-                    dep -= 1
-                    self.euler.append(self.parent[v])
-                    self.depth.append((dep << 32) + self.parent[v])
-                stk.pop()
+                last[node] = len(euler)
+                if parent[node] != -1:
+                    euler.append(parent[node])
                 stk.pop()
 
-    def lca(self, u, v):
-        left = min(self.first[u], self.first[v])
-        right = max(self.first[u], self.first[v])
-        return self.st.query(left, right + 1) & ((1 << 32) - 1)
+    first = [-1 for _ in range(n)]
+    last = [-1 for _ in range(n)]
+    parent = [-1 for _ in range(n)]
+    euler = []
+
+    if rt is None:
+        all(bfs(node) for node in range(n) if parent[node] == -1)
+    else:
+        bfs(rt)
+
+    return first, last, euler
