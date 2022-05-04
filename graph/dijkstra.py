c1853fa1d97fa35396b32f06616e2b3be6d90ae8
kuanhungchen <knhngchn@gmail.com>
2022-05-05 03:32:01 +0800

    fix(dijkstra): minor fix

--- graph/dijkstra.py
+++ graph/dijkstra.py
@@ -1,13 +1,15 @@
+from heapq import heappush, heappop
+
 def dijkstra(n, G, start):
+    # O(V + ElogV)
     dist = [-1 for _ in range(n)]
     prev = [-1 for _ in range(n)]
     dist[start] = 0
     pq = [(0, start)]  # (distance, node)
     while pq:
         d, node = heappop(pq)
-        if d > dist[node]:
+        if d != dist[node]:
             continue
-        d = dist[node]
         for (neigh, neigh_d) in G[node]:
             if dist[neigh] == -1 or d + neigh_d < dist[neigh]:
                 dist[neigh] = d + neigh_d
