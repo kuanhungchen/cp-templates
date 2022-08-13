069504f865190cb509bd88d265b69269b0d83445
kuanhungchen <knhngchn@gmail.com>
2022-08-13 13:18:25 +0800

    perf(is_prime): minor optimization

--- number/is_prime.py
+++ number/is_prime.py
@@ -1,8 +1,9 @@
 def is_prime(x):
-    if x == 2: return True
-    if x == 1 or x & 1 == 0: return False
-    k = 3
+    if x == 2 or x == 3: return True
+    if x == 1 or x % 2 == 0 or x % 3 == 0: return False
+    k, step = 5, 2
     while k * k <= x:
         if x % k == 0: return False
-        k += 2
+        k += step
+        step = 6 - step
     return True
