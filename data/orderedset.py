e6fb353e16f9cddcd8087bbea74324a12eedf5d5
kuanhungchen <knhngchn@gmail.com>
2022-08-22 16:29:41 +0800

    doc(orderedset): add docstring

--- data/orderedset.py
+++ data/orderedset.py
@@ -1,5 +1,8 @@
+from fenwick import Fenwick
+
 class OrderedSet:
     def __init__(self, n, nums=None):
+        # nums should be frequency array
         self.n = n
         self.fenw = Fenwick(n, nums)
 
@@ -19,13 +22,18 @@ class OrderedSet:
         self.fenw.add(val, -1)
         return True
 
+    def count(self, val):
+        return self.fenw.query(val, val + 1)
+
     def get(self, idx):
+        # return value with given rank, 0-indexed and can be negative
         sz = len(self)
         if not (-sz <= idx < sz): return -1
         if idx < 0: return self.get(sz + idx)
         return self.fenw.bisect_left(idx + 1)
 
     def rank(self, val):
+        # return rank of given value which should have already existed
         if val not in self: return -1
         return self.fenw.pref(val)
 
@@ -45,11 +53,11 @@ class OrderedSet:
         return self.get(0)
 
     def pre(self, val):
-        # largest value less than or equal to val
+        # return largest value less than or equal to val
         ans = self.fenw.bisect_left(self.fenw.pref(val + 1))
         return ans
 
     def nxt(self, val):
-        # smallest value greater than or equal to val
+        # return smallest value greater than or equal to val
         ans = self.fenw.bisect_right(self.fenw.pref(val))
         return ans if ans != self.n else -1
