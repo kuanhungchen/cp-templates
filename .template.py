60a4df75bdb1819917cf88018313ac38a6f58be2
Hank Chen <hankchen114@gmail.com>
2023-01-25 19:09:30 +0800

    feat: show line number while debugging

--- .template.py
+++ .template.py
@@ -4,13 +4,17 @@ from collections    import defaultdict, deque, Counter
 from itertools      import accumulate, combinations, permutations, product
 from functools      import lru_cache, cmp_to_key, reduce
 from heapq          import heapify, heappush, heappop, heappushpop, heapreplace
+from inspect        import currentframe
 # from pypyjit        import set_param
 # set_param("max_unroll_recursion=-1")
 # setrecursionlimit(300005)
 INF   = 1 << 60
-MOD   = 998244353 + 1755654
+MOD   = 1000000007
+MOD2  = 998244353
 input = lambda: stdin.readline().rstrip("\r\n")
+linen = lambda cf_: "[L" + str(cf_.f_back.f_lineno) + "] "
 dbg   = lambda *A, **M: stderr.write("\033[91m" + \
+        ("" if not M.get("lineno", True) else linen(currentframe()) + \
         M.get("sep", " ").join(map(str, A)) + M.get("end", "\n") + "\033[0m")
 # ============================ START OF MY CODE ============================ #
 
@@ -21,5 +25,5 @@ def solve(_tc):
 if __name__ == "__main__":
     _tcs = int(input())
     for _tc in range(1, vars().get("_tcs", 1) + 1):
-        dbg("=== Case {} ===".format(str(_tc).rjust(2)))
+        dbg("=== Case {} ===".format(str(_tc).rjust(2)), lineno=False)
         solve(_tc)
