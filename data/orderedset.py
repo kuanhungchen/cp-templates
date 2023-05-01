65742c9a24e1e0695cf1199edca3696f1fbdfba6
kuanhungchen <hankchen114@gmail.com>
2023-05-02 01:55:11 +0800

    feat(orderedset): major update

--- data/orderedset.py
+++ data/orderedset.py
@@ -1,48 +1,25 @@
 from fenwick import Fenwick
 
+
 class OrderedSet:
-    def __init__(self, n, nums=None):
-        # nums should be frequency array
-        self.n = n
-        self.fenw = Fenwick(n, nums)
+    def __init__(self, maxn, nums=None):
+        # nums should be unique
+        self._maxn = maxn # nums can be only [0, maxn - 1]
+        self._sz = 0
+        if nums:
+            self._sz = len(nums)
+            freq = [0 for _ in range(maxn)]
+            for num in nums: freq[num] += 1
+            self._fenw = Fenwick(maxn, freq)
+        else:
+            self._fenw = Fenwick(maxn)
 
     def __contains__(self, val):
-        return bool(self.fenw.query(val, val + 1))
+        self._check(val)
+        return bool(self._fenw.query(val, val + 1))
 
     def __len__(self):
-        return self.fenw.pref(self.n)
-
-    def add(self, val):
-        if val in self: return False
-        self.fenw.add(val, 1)
-        return True
-
-    def remove(self, val):
-        if val not in self: return False
-        self.fenw.add(val, -1)
-        return True
-
-    def count(self, val):
-        return self.fenw.query(val, val + 1)
-
-    def get(self, idx):
-        # return value with given rank, 0-indexed and can be negative
-        sz = len(self)
-        if not (-sz <= idx < sz): return -1
-        if idx < 0: return self.get(sz + idx)
-        return self.fenw.bisect_left(idx + 1)
-
-    def rank(self, val):
-        # return rank of given value which should have already existed
-        if val not in self: return -1
-        return self.fenw.pref(val)
-
-    def median(self, ceil=False):
-        sz = len(self)
-        if sz == 0: return -1
-        if sz % 2 == 1 or ceil:
-            return self.get(sz // 2)
-        return self.get((sz - 1) // 2)
+        return self._sz
 
     @property
     def max(self):
@@ -52,12 +29,57 @@ class OrderedSet:
     def min(self):
         return self.get(0)
 
+    def median(self, ceil=False):
+        # return median of elements, or -1 if not exists
+        if self._sz & 1 or ceil: return self.get(self._sz // 2)
+        return self.get((self._sz - 1) // 2)
+
+    def add(self, val):
+        self._check(val)
+        if val in self: return False
+        self._fenw.add(val, 1)
+        self._sz += 1
+        return True
+
+    def remove(self, val):
+        self._check(val)
+        if val not in self: return False
+        self._fenw.add(val, -1)
+        self._sz -= 1
+        return True
+
+    def get(self, rnk):
+        # return element of rnk (0-indexed), or -1 if not exists
+        if not -self._sz <= rnk < self._sz: return -1
+        if rnk < 0: return self.get(self._sz + rnk)
+        return self._fenw.bisect_left(rnk + 1)
+
+    def rank(self, val):
+        # return the rank of val (0-indexed), or -1 if not exists
+        self._check(val)
+        if val not in self: return -1
+        return self._fenw.pref(val)
+
     def pre(self, val):
-        # return largest value less than or equal to val
-        ans = self.fenw.bisect_left(self.fenw.pref(val + 1))
+        # reutrn first element <= val, or -1 if not exists
+        self._check(val)
+        ans = self._fenw.bisect_left(self._fenw.pref(val + 1))
         return ans
 
     def nxt(self, val):
-        # return smallest value greater than or equal to val
-        ans = self.fenw.bisect_right(self.fenw.pref(val))
-        return ans if ans != self.n else -1
+        # return first element >= val, or -1 if not exists
+        self._check(val)
+        ans = self._fenw.bisect_right(self._fenw.pref(val))
+        return ans if ans != self._maxn else -1
+
+    def _check(self, val):
+        if not 0 <= val < self._maxn:
+            raise ValueError("{} not in [0, {}]".format(val, self._maxn - 1))
+
+    def __repr__(self):
+        return "OrderedSet({})".format(list(self))
+
+    def __iter__(self):
+        for val in range(self._maxn):
+            if val in self:
+                yield val
