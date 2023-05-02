7437e57a8c58151ed29c1ca4321e4e68182f49e0
kuanhungchen <hankchen114@gmail.com>
2023-05-02 08:54:45 +0800

    style(orderedset): add function argument types and default values

--- data/orderedset.py
+++ data/orderedset.py
@@ -1,8 +1,9 @@
 from fenwick import Fenwick
+from typing import Iterator, List
 
 
 class OrderedSet:
-    def __init__(self, maxn, nums=None):
+    def __init__(self, maxn: int, nums: List[int] | None = None):
         self._maxn = maxn  # nums can be only [0, maxn - 1]
         self._freq = [0 for _ in range(maxn)]
         if nums:
@@ -12,27 +13,27 @@ class OrderedSet:
         self._fenw = Fenwick(maxn, self._freq)
         self._sz = self._fenw.pref(maxn)
 
-    def __contains__(self, val):
+    def __contains__(self, val: int) -> bool:
         self._check(val)
         return bool(self._freq[val])
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self._sz
 
     @property
-    def max(self):
+    def max(self) -> int:
         return self.kth(-1)
 
     @property
-    def min(self):
+    def min(self) -> int:
         return self.kth(0)
 
-    def median(self, ceil=False):
+    def median(self, ceil: bool = False) -> int:
         # return median of elements, or -1 if not exists
         if self._sz & 1 or ceil: return self.kth(self._sz // 2)
         return self.kth((self._sz - 1) // 2)
 
-    def add(self, val):
+    def add(self, val: int) -> bool:
         self._check(val)
         if val in self: return False
         self._fenw.add(val, 1)
@@ -40,7 +41,7 @@ class OrderedSet:
         self._sz += 1
         return True
 
-    def remove(self, val):
+    def remove(self, val: int) -> bool:
         self._check(val)
         if val not in self: return False
         self._fenw.add(val, -1)
@@ -48,45 +49,45 @@ class OrderedSet:
         self._sz -= 1
         return True
 
-    def kth(self, k):
+    def kth(self, k: int) -> int:
         # return element of rank k (0-indexed), or -1 if not exists
         if not -self._sz <= k < self._sz: return -1
         if k < 0: return self.kth(self._sz + k)
         return self._fenw.bisect_left(k + 1)
 
-    def rank(self, val):
+    def rank(self, val: int) -> int:
         # return the rank of val (0-indexed), or -1 if not exists
         self._check(val)
         if val not in self: return -1
         return self._fenw.pref(val)
 
-    def pre(self, val):
+    def pre(self, val: int) -> int:
         # reutrn first element <= val, or -1 if not exists
         self._check(val)
         ans = self._fenw.bisect_left(self._fenw.pref(val + 1))
         return ans
 
-    def nxt(self, val):
+    def nxt(self, val: int) -> int:
         # return first element >= val, or -1 if not exists
         self._check(val)
         ans = self._fenw.bisect_right(self._fenw.pref(val))
         return ans if ans != self._maxn else -1
 
-    def _check(self, val):
+    def _check(self, val: int) -> None:
         if not 0 <= val < self._maxn:
             raise ValueError("{} not in [0, {}]".format(val, self._maxn - 1))
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "OrderedSet({})".format(list(self))
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[int]:
         for val in range(self._maxn):
             for _ in range(self._freq[val]):
                 yield val
 
 
 class OrderedMultiSet(OrderedSet):
-    def __init__(self, maxn, nums=None):
+    def __init__(self, maxn: int, nums: List[int] | None = None):
         self._maxn = maxn  # nums can be only [0, maxn - 1]
         self._freq = [0 for _ in range(maxn)]
         if nums:
@@ -96,12 +97,12 @@ class OrderedMultiSet(OrderedSet):
         self._fenw = Fenwick(maxn, self._freq)
         self._sz = self._fenw.pref(maxn)
 
-    def count(self, val):
+    def count(self, val: int) -> int:
         # return occurence of val
         self._check(val)
         return self._fenw.query(val, val + 1)
 
-    def update(self, val, delta):
+    def update(self, val: int, delta: int) -> bool:
         # add or remove occurence of val
         self._check(val)
         if self.count(val) + delta < 0: return False
@@ -110,8 +111,8 @@ class OrderedMultiSet(OrderedSet):
         self._sz += delta
         return True
 
-    def add(self, val):
-        self.update(val, 1)
+    def add(self, val: int) -> bool:
+        return self.update(val, 1)
 
-    def remove(self, val):
-        self.update(val, -1)
+    def remove(self, val: int) -> bool:
+        return self.update(val, -1)
