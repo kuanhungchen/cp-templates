3cd179f7f7280ae70926621010c362d93a93db27
kuanhungchen <hankchen114@gmail.com>
2023-05-07 11:25:29 +0800

    feat(dsu): add DSUWithData)

--- data/dsu.py
+++ data/dsu.py
@@ -1,41 +1,73 @@
 class DSU:
     def __init__(self, n):
-        self.rt = [-1 for _ in range(n)]
-        self.n = n
-        self.sz = n  # number of sets
+        self._n = n
+        self._sz = n
+        self._root = [-1 for _ in range(n)]
 
-    def size(self, x): return - self.rt[self.find(x)]
+    @property
+    def size(self):
+        return self._sz
 
-    def same(self, x, y): return self.find(x) == self.find(y)
+    @property
+    def roots(self):
+        return [i for i, x in enumerate(self._root) if x < 0]
 
-    def group_count(self): return self.sz
+    @property
+    def groups(self):
+        dic = {r: [] for r in self.roots}
+        for i in range(self._n):
+            dic[self.find(i)].append(i)
+        return dic
 
-    def find(self, x):
-        if self.rt[x] < 0:
+    def group_size(self, x: int):
+        return - self._root[self.find(x)]
+
+    def find(self, x: int):
+        if self._root[x] < 0:
             return x
-        self.rt[x] = self.find(self.rt[x])
-        return self.rt[x]
+        self._root[x] = self.find(self._root[x])
+        return self._root[x]
 
-    def union(self, x, y):
+    def same(self, x: int, y: int):
+        return self.find(x) == self.find(y)
+
+    def union(self, x: int, y: int):
         x, y = self.find(x), self.find(y)
         if x == y:
             return False
-        if self.rt[x] > self.rt[y]:
+        if self._root[x] > self._root[y]:
             x, y = y, x
-        self.rt[x] += self.rt[y]
-        self.rt[y] = x
-        self.sz -= 1
+        self._root[x] += self._root[y]
+        self._root[y] = x
+        self._sz -= 1
         return True
 
-    def members(self, x):
+    def members(self, x: int):
         root = self.find(x)
-        return [i for i in range(self.n) if self.find(i) == root]
+        return [i for i in range(self._n) if self.find(i) == root]
 
-    def roots(self):
-        return [i for i, x in enumerate(self.rt) if x < 0]
 
-    def all_groups(self):
-        dic = {r: [] for r in self.roots()}
-        for i in range(self.n):
-            dic[self.find(i)].append(i)
-        return dic
+class DSUWithData(DSU):
+    def __init__(self, n, nums):
+        super(DSUWithData, self).__init__(n)
+        self._data = nums.copy()
+
+    @property
+    def data(self):
+        return self._data
+
+    def group_data(self, x: int):
+        return self._data[self.find(x)]
+
+    def union(self, x: int, y: int):
+        x, y = self.find(x), self.find(y)
+        if x == y:
+            return False
+        if self._root[x] > self._root[y]:
+            x, y = y, x
+        self._root[x] += self._root[y]
+        self._data[x] += self._data[y]
+        self._data[y] = 0
+        self._root[y] = x
+        self._sz -= 1
+        return True
