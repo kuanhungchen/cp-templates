70b493cb65b45b50d955de02d1dab497616ee3c4
Hank Chen <hankchen114@gmail.com>
2026-09-03 10:37:30 +0800

    feat(number): add lucy's algorithm for counting primes

--- number/sieve.py
+++ number/sieve.py
@@ -5,7 +5,7 @@ def eratosthenes(n):
     i = 2
     while i * i <= n:
         if is_prime[i]:
-            for j in range(2 * i, n + 1, i):
+            for j in range(i * i, n + 1, i):
                 is_prime[j] = False
         i += 1
     primes = [i for i, flag in enumerate(is_prime) if flag]
@@ -26,17 +26,40 @@ def linear_sieve(n):
     return primes, min_facts
 
 def prime_factors(x, min_facts):
-    # O(logn)
+    # O(log x)
     facts = []
-    while min_facts[x] != 1:
+    while x > 1:
         facts.append(min_facts[x])
         x //= min_facts[x]
     return facts
 
 
+def lucy(n):
+    # Returns number of primes <= n, O(n^(3/4))
+    if n < 2:
+        return 0
+    sqrt = int(n ** 0.5)
+    keys = []
+    for i in range(1, sqrt + 1):
+        keys.append(i)
+    for i in range(sqrt - (1 if keys[-1] == n // sqrt else 0), 0, -1):
+        keys.append(n // i)
+
+    st = {k: k - 1 for k in keys}
+    for p in range(2, sqrt + 1):
+        if st[p] > st[p - 1]:
+            sp1 = st[p - 1]
+            p2 = p * p
+            for k in reversed(keys):
+                if k < p2:
+                    break
+                st[k] -= st[k // p] - sp1
+    return st[n]
+
+
 def segment_sieve(L, R):
     # [L, R]
-    # O((R - L + 1)loglogR + sqrt(R)loglog(sqrt(R)))
+    # O((R - L + 1)loglogR + sqrt(R))
     # is_prime[idx] = True means the value `idx` is a prime.
     primes, _ = linear_sieve(int(R ** 0.5))
 
