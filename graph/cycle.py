928550d45071bb81cdc97defdedbad691d9db037
Hank Chen <hankchen114@gmail.com>
2026-09-03 23:57:09 +0800

    feat(graph): add cycle detection for undirected graph

--- graph/cycle.py
+++ graph/cycle.py
@@ -27,8 +27,40 @@ def is_cyclic(n, G):
     for i in range(n):
         if state[i] == 0:
             if dfs(i):
-                return cycle
+                return cycle[::-1]
     return []
 
-adj = [[(1, 0)], [(2, 1)], [(1, 2)]]
-print(is_cyclic(3, adj))
+
+def is_cyclic_undirected(n, G):
+    # Returns a cycle (edge IDs) if undirected graph is cyclic.
+    def dfs(node, prev_e):
+        nonlocal cycle_start
+        state[node] = -1
+
+        for (neigh, edge_i) in G[node]:
+            if edge_i == prev_e:
+                continue
+            if state[neigh] == -1:
+                cycle_start = neigh
+                cycle.append(edge_i)
+                return True
+
+            if state[neigh] == 0:
+                if dfs(neigh, edge_i):
+                    if cycle_start is not None:
+                        cycle.append(edge_i)
+                        if node == cycle_start:
+                            cycle_start = None
+                    return True
+
+        state[node] = 1
+        return False
+
+    cycle = []
+    cycle_start = None
+    state = [0 for _ in range(n)]
+    for i in range(n):
+        if state[i] == 0:
+            if dfs(i, -1):
+                return cycle[::-1]
+    return []
