38571a4b8e4cc67142da8efe1bdc7a7612b3ddb6
Hank Chen <hankchen114@gmail.com>
2026-09-03 23:23:38 +0800

    feat(cycle): fix bugs in cycle detection

--- graph/cycle.py
+++ graph/cycle.py
@@ -1,18 +1,34 @@
 def is_cyclic(n, G):
-    # return a cycle (edge ids) if graph is cyclic
+    # Returns a cycle (edge IDs) if graph is cyclic.
     def dfs(node):
-        if state[node] == 0:
-            state[node] = -1
-            for (neigh, edge_i) in G[node]:
-                if dfs(neigh) == -1:
-                    cycle.append(edge_i)
-                    return -1
-            state[node] = 1
-        return state[node]
+        nonlocal cycle_start
+        state[node] = -1
+
+        for (neigh, edge_i) in G[node]:
+            if state[neigh] == -1:
+                cycle_start = neigh
+                cycle.append(edge_i)
+                return True
+
+            if state[neigh] == 0:
+                if dfs(neigh):
+                    if cycle_start is not None:
+                        cycle.append(edge_i)
+                        if node == cycle_start:
+                            cycle_start = None
+                    return True
+
+        state[node] = 1
+        return False
 
     cycle = []
+    cycle_start = None
     state = [0 for _ in range(n)]
     for i in range(n):
-        dfs(i)
+        if state[i] == 0:
+            if dfs(i):
+                return cycle
+    return []
 
-    return cycle
+adj = [[(1, 0)], [(2, 1)], [(1, 2)]]
+print(is_cyclic(3, adj))
