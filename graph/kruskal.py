57f855a5ac700b1eceb4d896f7337461ac32753e
Hank Chen <hankchen114@gmail.com>
2026-09-04 18:31:50 +0800

    feat(graph): support producing MST in prim and kruskal

--- graph/kruskal.py
+++ graph/kruskal.py
@@ -5,10 +5,27 @@ def kruskal(n, edges):
     # O(ElogE)
     edges.sort(key=lambda e: e[2])
     dsu = DSU(n)
-    res = 0
-    for (u, v, c) in edges:
+    total_w = 0
+    for (u, v, w) in edges:
         if dsu.union(u, v):
-            res += c
+            total_w += w
         if dsu.size == 1:
             break
-    return res if dsu.size == 1 else -1
+    return total_w if dsu.size == 1 else -1
+
+
+def kruskal_with_mst(n, edges):
+    # O(ElogE)
+    indexed_edges = [(u, v, w, idx) for idx, (u, v, w) in enumerate(edges)]
+    indexed_edges.sort(key=lambda e: e[2])  # sort by weight
+
+    dsu = DSU(n)
+    total_w = 0
+    mst = []
+    for (u, v, w, idx) in indexed_edges:
+        if dsu.union(u, v):
+            total_w += w
+            mst.append(idx)
+        if dsu.size == 1:
+            break
+    return (total_w, mst) if dsu.size == 1 else (-1, [])
--- graph/prim.py
+++ graph/prim.py
@@ -1,20 +1,54 @@
+from heapq import heappop, heappush
+
+INF = 1 << 60
+
+
 def prim_dense(n, mat):
     # O(V^2)
     used = [False for _ in range(n)]
     dist = [INF for _ in range(n)]
     dist[0] = 0
-    ans = cnt = 0
+    total_w = cnt = 0
     for _ in range(n):
         min_i = -1
         for i in range(n):
             if not used[i] and (min_i == -1 or dist[i] < dist[min_i]):
                 min_i = i
+
+        if dist[min_i] == INF: return -1
+
         used[min_i] = True
-        ans += dist[min_i]; cnt += 1
+        total_w += dist[min_i]; cnt += 1
         for i in range(n):
             if not used[i]:
                 dist[i] = min(dist[i], mat[min_i][i])
-    return ans if cnt == n else -1
+    return total_w if cnt == n else -1
+
+def prim_dense_with_mst(n, mat, edge_ids):
+    # O(V^2)
+    used = [False for _ in range(n)]
+    dist = [INF for _ in range(n)]
+    par_edges = [-1 for _ in range(n)]
+    dist[0] = 0
+    total_w = cnt = 0
+    mst = []
+    for _ in range(n):
+        min_i = -1
+        for i in range(n):
+            if not used[i] and (min_i == -1 or dist[i] < dist[min_i]):
+                min_i = i
+
+        if dist[min_i] == INF: return -1, []
+
+        used[min_i] = True
+        total_w += dist[min_i]; cnt += 1
+        if par_edges[min_i] != -1: mst.append(par_edges[min_i])
+        for i in range(n):
+            if not used[i] and mat[min_i][i] < dist[i]:
+                dist[i] = mat[min_i][i]
+                par_edges[i] = edge_ids[min_i][i]
+    return (total_w, mst) if cnt == n else (-1, [])
+
 
 def prim(n, G):
     # O(ElogV)
@@ -31,3 +65,22 @@ def prim(n, G):
             if not used[neigh]:
                 heappush(pq, (neigh_w, neigh))
     return ans if cnt == n else -1
+
+def prim_with_mst(n, G):
+    # O(ElogV)
+    used = [False for _ in range(n)]
+    pq = [(0, 0, -1)]  # (weight, node, edge_i)
+    total_w = cnt = 0
+    mst = []
+    while pq:
+        w, node, ei = heappop(pq)
+        if used[node]:
+            continue
+        used[node] = True
+        total_w += w; cnt += 1
+        if ei != -1:
+            mst.append(ei)
+        for (neigh, neigh_w, edge_i) in G[node]:
+            if not used[neigh]:
+                heappush(pq, (neigh_w, neigh, edge_i))
+    return (total_w, mst) if cnt == n else (-1, [])
