5da0a62e1048f93b8af0cd6a0e01e3c7a08cf2f2
Hank Chen <hankchen114@gmail.com>
2026-09-06 17:54:41 +0800

    feat(data): extend sorted list to sorted set and dict

--- data/sortedlist.py
+++ data/sortedlist.py
@@ -2,14 +2,14 @@ from math import ceil, sqrt
 from bisect import bisect_left, bisect_right
 
 
-class SortedSet:
+class SortedList:
     BUCKET_RATIO = 16
-    SPLIT_RATIO = 32
+    SPLIT_RATIO = 24
 
     def __init__(self, nums=None):
-        sorted_unique = sorted(set(nums if nums is not None else []))
-        self._size = len(sorted_unique)
-        self._build(sorted_unique)
+        sorted_nums = sorted(nums) if nums is not None else []
+        self._size = len(sorted_nums)
+        self._build(sorted_nums)
 
     def _build(self, sorted_elements: list) -> None:
         n = self._size
@@ -34,8 +34,8 @@ class SortedSet:
         return self._size
 
     def __eq__(self, other) -> bool:
-        if isinstance(other, SortedSet):
-            return list(self) == list(other)
+        if isinstance(other, SortedList):
+            return len(self) == len(other) and list(self) == list(other)
         return False
 
     def __str__(self) -> str:
@@ -55,20 +55,17 @@ class SortedSet:
         bucket, _, i = self._find(x)
         return i < len(bucket) and bucket[i] == x
 
-    def add(self, x) -> bool:
+    def add(self, x) -> None:
         if self._size == 0:
             self._buckets = [[x]]
             self._size = 1
-            return True
+            return
         bucket, bi, i = self._find(x)
-        if i < len(bucket) and bucket[i] == x:
-            return False
         bucket.insert(i, x)
         self._size += 1
         if len(bucket) > len(self._buckets) * self.SPLIT_RATIO:
             mid = len(bucket) >> 1
             self._buckets[bi: bi + 1] = [bucket[:mid], bucket[mid:]]
-        return True
 
     def discard(self, x) -> bool:
         if self._size == 0:
@@ -153,3 +150,145 @@ class SortedSet:
     def irange(self, left, right) -> range:
         # Returns the index range of elemetns in [left, right].
         return range(self.bisect_left(left), self.bisect_right(right))
+
+
+class SortedSet:
+    def __init__(self, nums=None):
+        unique = sorted(set(nums)) if nums is not None else []
+        self._list = SortedList(unique)
+
+    def add(self, x) -> bool:
+        if x in self._list:
+            return False
+        self._list.add(x)
+        return True
+
+    def discard(self, x) -> bool:
+        return self._list.discard(x)
+
+    def count(self, x) -> int:
+        return 1 if x in self._list else 0
+
+    def bisect_left(self, x) -> int:
+        return self._list.bisect_left(x)
+
+    def bisect_right(self, x) -> int:
+        return self._list.bisect_right(x)
+
+    def lt(self, x):
+        return self._list.lt(x)
+
+    def le(self, x):
+        return self._list.le(x)
+
+    def gt(self, x):
+        return self._list.gt(x)
+
+    def ge(self, x):
+        return self._list.ge(x)
+
+    def irange(self, left, right):
+        return self._list.irange(left, right)
+
+    def pop(self, i=-1):
+        return self._list.pop(i)
+
+    def __getitem__(self, i):
+        return self._list[i]
+
+    def __len__(self) -> int:
+        return len(self._list)
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, SortedSet):
+            return len(self) == len(other) and list(self) == list(other)
+        return False
+
+    def __contains__(self, x):
+        return x in self._list
+
+    def __iter__(self):
+        return iter(self._list)
+
+    def __reversed__(self):
+        return reversed(self._list)
+
+    def __str__(self):
+        return f"SortedSet({list(self)})"
+
+    __repr__ = __str__
+
+
+class SortedDict:
+    def __init__(self, pairs=None):
+        self._map = {}
+        self._keys = SortedList()
+        if pairs:
+            for k, v in pairs:
+                self[k] = v
+
+    def __setitem__(self, key, value):
+        if key not in self._map:
+            self._keys.add(key)
+        self._map[key] = value
+
+    def __getitem__(self, key):
+        return self._map[key]
+
+    def __delitem__(self, key):
+        if key in self._map:
+            del self._map[key]
+            self._keys.discard(key)
+
+    def pop(self, key, default=...):
+        if key in self._map:
+            val = self._map.pop(key)
+            self._keys.discard(key)
+            return val
+        if default is not ...:
+            return default
+        raise KeyError(key)
+
+    def peekitem(self, index=-1):
+        key = self._keys[index]
+        return key, self._map[key]
+
+    def keys(self):
+        return self._keys
+
+    def values(self):
+        for k in self._keys:
+            yield self._map[k]
+
+    def items(self):
+        for k in self._keys:
+            yield k, self._map[k]
+
+    def bisect_left(self, key) -> int:
+        return self._keys.bisect_left(key)
+
+    def bisect_right(self, key) -> int:
+        return self._keys.bisect_right(key)
+
+    def __contains__(self, key):
+        return key in self._map
+
+    def __len__(self):
+        return len(self._map)
+
+    def __eq__(self, other) -> bool:
+        if isinstance(other, SortedDict):
+            return self._map == other._map
+        return False
+
+    def __iter__(self):
+        return iter(self._keys)
+
+    def __reversed__(self):
+        return reversed(self._keys)
+
+    def __str__(self):
+        items_str = ", ".join(f"{k!r}: {self._map[k]!r}" for k in self._keys)
+        return f"SortedDict({{{items_str}}})"
+
+    __repr__ = __str__
